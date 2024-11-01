51e23f3b8a82160a371c7d9de10574ce56b61f6a
yakhsyaa23 <88273148+yakhsyaa23@users.noreply.github.com>
2024-11-01 14:43:58 +0800

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -22,6 +22,7 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
+            name="yasa1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
